2d962b48d49ce0dc77a135da0dd884310320d17d
sukesan000 <oyama.k.1997@gmail.com>
2022-05-17 19:40:40 +0900

    first commit

---
+++ src/main/java/com/example/seppan/SeppanApplication.java
@@ -0,0 +1,13 @@
+package com.example.seppan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeppanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeppanApplication.class, args);
+	}
+
+}
