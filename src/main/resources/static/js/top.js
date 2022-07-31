be3b66f492dcf6eddabba8694173065532fbd01e
sukesan000 <oyama.k.1997@gmail.com>
2022-07-31 16:50:51 +0900

    フォームバリデーション追加

--- src/main/resources/static/js/top.js
+++ src/main/resources/static/js/top.js
@@ -28,6 +28,28 @@ $(function(){
             remarks: $("#edit_remarks").val()
         }
 
+        const {money, categoryId, date, payerId, remarks} = EventInfo;
+        console.log(date.match(/\d{4}-\d{2}-\d{2}/));
+        let message = [];
+        if(money == ""){
+            message.push("金額が未入力です");
+        }else if(isNaN(money)){
+            message.push("数値を入力してください");
+        }
+        if(date == ""){
+            message.push("日付が未入力です");
+        }else if(!date.match(/\d{4}-\d{2}-\d{2}/)){
+            message.push("有効な日付で入力してください");
+        }
+        if(remarks.length >= 21){
+            message.push("備考は20文字以内で記入してください");
+        }
+
+        if(message.length > 0){
+            alert(message);
+            return;
+        }
+
         $.ajax({
               url: "/seppan/top/api/editEvent",  // リクエストを送信するURLを指定（action属性のurlを抽出）
               type: "POST",  // HTTPメソッドを指定（デフォルトはGET）
